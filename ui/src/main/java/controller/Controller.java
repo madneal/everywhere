05d9c50211d785252824239f2541ad09c3525dbd
neal1991 <neal1991@sina.com>
2017-10-31 22:38:59 +0800

    add Controller

--- ui/src/main/java/controller/Controller.java
+++ ui/src/main/java/controller/Controller.java
@@ -1,4 +1,19 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField textField;
+
+    textField.textProperty().addListener(new ChangeListener<String>() {
+        @Override
+        public void changed(ObservableValue<? extends String> observable,
+                String oldValue, String newValue) {
+
+            outputTextArea.appendText("TextField Text Changed (newValue: " + newValue + ")\n");
+        }
+    });
 }
