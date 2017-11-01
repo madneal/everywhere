279a509201209380808aef5ed02f1977ca689320
neal1991 <neal1991@sina.com>
2017-11-01 19:37:08 +0800

    add inputchange

--- ui/src/main/java/controller/Controller.java
+++ ui/src/main/java/controller/Controller.java
@@ -1,19 +1,15 @@
 package controller;
 
-import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
 
 public class Controller {
     @FXML
     private TextField textField;
 
-    textField.textProperty().addListener(new ChangeListener<String>() {
-        @Override
-        public void changed(ObservableValue<? extends String> observable,
-                String oldValue, String newValue) {
 
-            outputTextArea.appendText("TextField Text Changed (newValue: " + newValue + ")\n");
-        }
-    });
+    public void getSearchTextChanged(InputMethodEvent event) {
+        System.out.println("txt:" + textField.getText());
+    }
 }
