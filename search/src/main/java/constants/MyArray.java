a21f1e4f656bc6cc5bf18d1569f218091b162e5f
neal1991 <neal1991@sina.com>
2017-12-05 21:16:43 +0800

    modify MyArray

--- search/src/main/java/constants/MyArray.java
+++ search/src/main/java/constants/MyArray.java
@@ -1,14 +1,20 @@
 package constants;
 
+import java.util.Arrays;
+
 public class MyArray<E> {
 
     private static final int DEFAULT_CAPACITY = 100;
+
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
 
     private static final Object[] EMPTY_ELMENT_DATA = {};
 
     private int size;
 
     private Object[] elementData;
+
+    private int modCount = 0;
 
     public MyArray(int capacity) {
         if (capacity > 0) {
@@ -18,6 +24,46 @@ public class MyArray<E> {
         } else {
             throw new IllegalArgumentException("Illegal capacity " + capacity);
         }
+    }
+
+    public boolean add(E e) {
+        ensureCapacityInternal(size + 1);
+        elementData[size++] = e;
+        return true;
+    }
+
+    private void ensureCapacityInternal(int minCapcity) {
+        if (elementData == EMPTY_ELMENT_DATA) {
+            minCapcity = Math.max(DEFAULT_CAPACITY, minCapcity);
+        }
+        ensureExplicitCapacity(minCapcity);
+    }
+
+    private void ensureExplicitCapacity(int minCapcity) {
+        modCount++;
+
+        if (minCapcity - elementData.length > 0) {
+            grow(minCapcity);
+        }
+    }
+
+    private void grow(int minCapacity) {
+        int oldCapcity = elementData.length;
+        int newCapacity = oldCapcity + 10;
+        if (newCapacity < minCapacity) {
+            newCapacity = minCapacity;
+        }
+        if (newCapacity > MAX_ARRAY_SIZE) {
+            newCapacity = hugeCapacity(minCapacity);
+        }
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) {
+            throw new OutOfMemoryError();
+        }
+        return (minCapacity > MAX_ARRAY_SIZE) ? Integer.MAX_VALUE : MAX_ARRAY_SIZE;
     }
 
 
