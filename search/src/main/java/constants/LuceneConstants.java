71238b7703e3f84d835748d3c3e74e630e61c6f5
madneal <bing.ecnu@gmail.com>
2025-06-22 22:09:13 +0800

    ready to clean

--- search/src/main/java/constants/LuceneConstants.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class LuceneConstants {
-
-    public static final String CONTENT = "content";
-    public static final String MODIFIED = "modified";
-    public static final String PATH = "path";
-}
--- ui/src/main/java/constants/Constants.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public class Constants {
-    public static String CONFIG_FILEPATH = "config.yaml";
-}
